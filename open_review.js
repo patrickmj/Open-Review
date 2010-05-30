545b2486e51b107d6b112bc401ff4a6ab9d7a6df
Patrick Murray-John <pgosetti@umw.edu>
2010-05-30 11:19:02 -0400

    added icons and editing for Drupal coding standards

--- open_review.js
+++ open_review.js
@@ -1,11 +1,34 @@
-var debug;
- 
+// $Id$
+
+/**
+ * @file
+ * Javascript for managing jQuery dialogs for viewing and writing comments 
+ * 
+ */
  
 var OpenReview = {
+	
+	/**
+	 * Array of open dialogs
+	 */
 	commentViewDialogs : [],
-	
 
-	
+    /**
+     * Setting to pass use during dialog creation
+     */
+
+    commentViewSettings: {
+        autoOpen: false,
+        draggable: true,
+        resizable: true,
+        focus: this.commentViewDialogFocus,
+        close: this.commentViewDialogClose
+    },
+		
+	/**
+	 * Opens a dialog for commenting on a paragraph
+	 * @param {Object} event
+	 */
 	openCommentDialog: function(event) {
 		//make a copy of the comment form?
 		var commentForm = $('#open-review-comment-form');
@@ -19,7 +42,10 @@ var OpenReview = {
 		d.dialog('open');
 		
 	},
-	
+	/**
+	 * Opens a dialog for viewing all the comments on a paragraph
+	 * @param {Object} event
+	 */
 	openViewDialog: function(event) {
 		var targetP = event.target.parentNode.parentNode;
 		var paraId = targetP.id;
@@ -39,9 +65,11 @@ var OpenReview = {
 		}
 	},
 	
-	
-	// Assuming elements with id="comment"
-	
+	/**
+	 * Gathers the comments that apply to a paragraph
+	 * @param string paraId
+	 * @return array Array of comment nodes
+	 */
 	gatherCommentsForPara: function(paraId) {
 		var returnArray = new Array();
 		var comments = $('.comment').each(function(index, el) {
@@ -54,6 +82,11 @@ var OpenReview = {
 		
 	},
 	
+	/**
+	 * Return the dialog to view comments on a paragraph, or create one if needed
+	 * @param string paraId
+	 * @return jQuery dialoge
+	 */
 	getCommentViewDialog: function(paraId) {
 		for(var i = 0; i<OpenReview.commentViewDialogs.length; i++) {
 			if(OpenReview.commentViewDialogs[i].data('paraId') == paraId) {
@@ -77,11 +110,20 @@ var OpenReview = {
 		return d;
 	},
 	
-	getParaSnippet: function(paraId) {
-		//this is brittle, because I'm trying to skip over the action spans
-		return '"' + $('#' + paraId).text().substr(8, 10) + '. . ."';
+	/**
+	 * Return a snippet from the paragraph
+	 * @param string paraId
+	 * @return string The first few characters of the paragraph
+	 */
+	getParaSnippet: function(paraId) {	
+		return '"' + $('#' + paraId).text().substr(0, 10) + '. . ."';
 	},
 	
+	/**
+	 * Callback for when a dialog receives focus
+	 * @param {Object} event
+	 * @param {Object} ui
+	 */
 	commentViewDialogFocus: function(event, ui) {
         for(var i = 0; i<OpenReview.commentViewDialogs.length; i++) {
             $(OpenReview.commentViewDialogs[i]).removeClass('open-review-active');
@@ -92,6 +134,11 @@ var OpenReview = {
 		$('#' + paraId).addClass('open-review-active');
 	},
 	
+	/**
+	 * Callback for when a dialog closes
+	 * @param {Object} event
+	 * @param {Object} ui
+	 */
 	commentViewDialogClose: function(event, ui) {
 		var paraId = $(event.target).data('paraId');
         
@@ -100,17 +147,8 @@ var OpenReview = {
                 OpenReview.commentViewDialogs.splice(i, 1);
             }
         }		
-	},
-	
-
+	}
 }
-OpenReview.commentViewSettings = {
-    autoOpen: false,
-    draggable: true,
-    resizable: true,
-    focus: OpenReview.commentViewDialogFocus,
-    close: OpenReview.commentViewDialogClose
-}   
 
  $(document).ready(function(){
     $('.open-review-para-comment').click(OpenReview.openCommentDialog);
